0540950dc391b9a69a0d75573e456bcfbab9005c
phl6 <guitarlouisli1997@gmail.com>
2021-06-23 17:33:15 +0800

    done ch.11 Creating a simple webserver, learnt to start server on localhost and listen to a port, receive request and return response.

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,16 @@
-// Ch.7 Using Modules 1: Core Modules
-const fs = require('fs');
+const fs = require('fs'); // Ch.7 Using Modules 1: Core Modules
+const http = require('http'); //Ch.11 Creating a simple webserver
+
+///////////////////////////
+//      FILES
+///////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Ch.8 Reading and Writing Files (Synchronous)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Blocking, Synchronous way
-// Ch.8 Reading and Writing Files (Synchronous)
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
 
@@ -10,22 +18,44 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File Written!');
 
-//Non-blocking, Asynchrnous way
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Ch.9 Reading and Writing Files Asynchronously
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if(err) return console.log('ERROR!!!');
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    console.log(data1);
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2); //data2 depends on data1
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3); 
+//Non-blocking, Asynchrnous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log('ERROR!!!');
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
-                console.log('Your file has been written!');
-            })
-        });
-    });
+//     console.log(data1);
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2); //data2 depends on data1
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3); 
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+//                 console.log('Your file has been written!');
+//             })
+//         });
+//     });
+// });
+
+// console.log('Will read file');
+
+///////////////////////////
+//      SERVER
+///////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Ch.11 Creating a Simple Webserver
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Creating a webserver and return response if there's request
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server');
 });
 
-console.log('Will read file');
+//keep running and listen requests from localhost port8000
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000');
+});
