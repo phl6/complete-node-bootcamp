f2643ff409dd6439defdedf6f7196e7e3df0c6ee
phl6 <guitarlouisli1997@gmail.com>
2021-06-23 17:54:30 +0800

    done ch12 - Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs'); // Ch.7 Using Modules 1: Core Modules
 const http = require('http'); //Ch.11 Creating a simple webserver
+const url = require('url');
 
 ///////////////////////////
 //      FILES
@@ -51,7 +52,27 @@ const http = require('http'); //Ch.11 Creating a simple webserver
 
 //Creating a webserver and return response if there's request
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // Ch.12 Routing
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+    }
+    else if(pathName === '/product'){
+        res.end('This is the PRODUCT');
+    }
+    else{
+        res.writeHead(404, {
+            //putting header here
+            'Content-type': 'text/html',
+            'my-own-header': 'Self-defined header',
+        });
+        res.end(`<h1>Page not found!</h1>`);
+    }
+    
+
     res.end('Hello from the server');
 });
 
@@ -59,3 +80,8 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to request on port 8000');
 });
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+// Ch.12 Routing
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+
