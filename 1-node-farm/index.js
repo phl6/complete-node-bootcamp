d539fbb5eb280b9b2568f2604655c955aa177c17
phl6 <guitarlouisli1997@gmail.com>
2021-06-24 17:57:10 +0800

    Setup Pretierrc

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -6,7 +6,6 @@ const url = require('url');
 //      FILES
 ///////////////////////////
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Ch.8 Reading and Writing Files (Synchronous)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -31,7 +30,7 @@ const url = require('url');
 //     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //         console.log(data2); //data2 depends on data1
 //         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-//             console.log(data3); 
+//             console.log(data3);
 
 //             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
 //                 console.log('Your file has been written!');
@@ -52,6 +51,10 @@ const url = require('url');
 
 // ch17 using Modules 2: Our Own Modules
 const replaceTemplate = require('./modules/replaceTemplate');
+// ch20 using Modules 3: 3rd Party Modules
+const slugify = require('slugify');
+const { stringify } = require('ini');
+const { SSL_OP_CRYPTOPRO_TLSEXT_BUG } = require('constants');
 
 //ch13
 //***top-level code will only be executed once when program starts, so using synchronous readfile here is not a problam
@@ -59,10 +62,25 @@ const replaceTemplate = require('./modules/replaceTemplate');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); //*** __dirname = current directory ***
 const dataObj = JSON.parse(data); //returns an array of all the objects in data.json
 
+//ch20 - using 3rd party module - slugify
+console.log(slugify('Fresh Avacados', { lower: true }));
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 //ch14-15 HTML TEMPLATING: Building and Filling the Templates
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 //ch.17 (moved to ./modules/replaceTemplate.js)
 // const replaceTemplate = (template, product) => {
@@ -80,61 +98,57 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 //     return output;
 // };
 
-
 //Creating a webserver and return response if there's request
 const server = http.createServer((req, res) => {
-    // console.log(req.url);
-    // console.log(url.parse(req.url, true)); //IMPORTANT, retrieve the request url, true = query mode
-    const {query, pathname} = url.parse(req.url, true); //get the query(includes id here) and pathname
-    
-    // Ch.12 Routing
-    //Overview Page
-    if(pathname === '/' || pathname === '/overview'){
-        res.writeHead(200, {'Content-type': 'text/html'});
+  // console.log(req.url);
+  // console.log(url.parse(req.url, true)); //IMPORTANT, retrieve the request url, true = query mode
+  const { query, pathname } = url.parse(req.url, true); //get the query(includes id here) and pathname
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); //.join('') joins all the array elements into an empty string, then return a markup with new cards
-        // console.log(cardsHtml);
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        
-        res.end(output);
-    }
-    //Product Page
-    else if(pathname === '/product'){
-        res.writeHead(200, {'Content-type': 'text/html'});
+  // Ch.12 Routing
+  //Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        console.log(query);
-        const product = dataObj[query.id]; //get the object that matches the id retrieves from query(url)
-        console.log(product);
-        const output = replaceTemplate(tempProduct, product); //render the product template
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(''); //.join('') joins all the array elements into an empty string, then return a markup with new cards
+    // console.log(cardsHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-        res.end(output);
-    }
-    //ch.13 building a very simple api
-    //Api 
-    else if(pathname === '/api'){
-        res.writeHead(200, {'Content-type': 'application/json'}); //specify it returns a JSON object
-        res.end(data);
-    }
-    //Not Found
-    else{
-        res.writeHead(404, {
-            //putting header here
-            'Content-type': 'text/html',
-            'my-own-header': 'Self-defined header',
-        });
+    res.end(output);
+  }
+  //Product Page
+  else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        res.end(`<h1>Page not found!</h1>`);
-    }
-    
+    console.log(query);
+    const product = dataObj[query.id]; //get the object that matches the id retrieves from query(url)
+    console.log(product);
+    const output = replaceTemplate(tempProduct, product); //render the product template
 
-    res.end('Hello from the server');
+    res.end(output);
+  }
+  //ch.13 building a very simple api
+  //Api
+  else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' }); //specify it returns a JSON object
+    res.end(data);
+  }
+  //Not Found
+  else {
+    res.writeHead(404, {
+      //putting header here
+      'Content-type': 'text/html',
+      'my-own-header': 'Self-defined header',
+    });
+
+    res.end(`<h1>Page not found!</h1>`);
+  }
+
+  res.end('Hello from the server');
 });
 
 //keep running and listen requests from localhost port8000
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to request on port 8000');
+  console.log('Listening to request on port 8000');
 });
-
-
-
-
