6e4dcada1021e59a483688926827382af9fb0da5
phl6 <louisli1997@gmail.com>
2021-06-27 18:18:54 +0800

    Done up to Ch56, Learnt GET, POST, PATCH, DELETE with Express

--- Sec6_Express_NatoursAPI/app.js
+++ Sec6_Express_NatoursAPI/app.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const express = require('express');
+const {
+    emitKeypressEvents
+} = require('readline');
 
 const app = express();
 app.use(express.json()); //ch53, express.json() is a middleware that handles incoming request data
@@ -46,16 +49,50 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+//Ch54 Responding to URL Parameters
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+
+    const id = +req.params.id; //turn obj to number
+    const tour = tours.find(el => el.id === id);
+    // console.log(tour);
+
+    //if no id or no tour, return immediately
+    if (id > tours.length || !tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    }
+
+    //id exists
+    res.status(200).json({ //Jsend JSON Formatting Standard
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+});
+
 //Ch53 Handling POST Requests
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body); 
 
     // adding id to the new object which is saved to our ficitonal data later
     const newId = tours[tours.length - 1].id + 1;
-    //Object.assign() creates a new obj by merging 2 existing objs, (here it assigns id into the req body which stores the new data)
-    const newTour = Object.assign({
-        id: newId
-    }, req.body);
+
+    // 2 ways to merge 2 objects
+    //1) Object.assign() creates a new obj by merging 2 existing objs, (here it assigns id into the req body which stores the new data)
+    // const newTour = Object.assign({
+    //     id: newId
+    // }, req.body);
+
+    //2) use spread operator
+    const newTour = {
+        id: newId,
+        ...req.body
+    };
+
     //add newTour to ficitional db - tours
     tours.push(newTour);
 
@@ -67,11 +104,44 @@ app.post('/api/v1/tours', (req, res) => {
                 tour: newTour
             }
         })
+    })
+    // res.send('Done'); //always need to send back response to complete req/res cycle
+});
 
+//Ch55 Handling PATCH Request
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+    if (+req.params.id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...'
+        }
     })
 
+});
 
-    // res.send('Done'); //always need to send back response to complete req/res cycle
+//Ch56 Handling DELETE Request
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if (+req.params.id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    }
+    //204 = no content
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+
 });
 
 const port = 3000;
